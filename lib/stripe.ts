145087f8d4a304614431ecde73b80437f1a284d9
Creeper343 <72136874+Creeper343@users.noreply.github.com>
2025-12-14 10:48:36 +0100

    Delete lib directory

--- lib/stripe.ts
+++
@@ -1,9 +0,0 @@
-import Stripe from 'stripe';
-
-// ÄNDERUNG HIER: Wir fügen einen Fallback hinzu (|| 'sk_test_dummy')
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy', {
-    apiVersion: '2025-09-30.clover' as any, // 'as any' verhindert Versions-Meckern
-    typescript: true,
-});
-
-export { stripe };
